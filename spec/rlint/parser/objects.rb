77d0f8a54fcee7bdc936ab06ceb72a6d6ba7101a
Yorick Peterse <yorickpeterse@gmail.com>
2012-09-03 22:29:57 +0200

    Tests for object attribute references.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- spec/rlint/parser/objects.rb
+++ spec/rlint/parser/objects.rb
@@ -1,6 +1,22 @@
 require File.expand_path('../../../helper', __FILE__)
 
 describe 'Rlint::Parser' do
+  it 'Parse the reference of an object attribute' do
+    code = <<-CODE
+obj = Struct.new(:name).new('Ruby')
+obj.name
+    CODE
+
+    token = Rlint::Parser.new(code).parse[1]
+
+    token.class.should == Rlint::Token::MethodToken
+    token.name.should  == 'name'
+
+    token.receiver.class.should == Rlint::Token::VariableToken
+    token.receiver.name.should  == 'obj'
+    token.receiver.type.should  == :local_variable
+  end
+
   it 'Parse the assignment of a value to an object attribute' do
     code = <<-CODE
 obj = Struct.new(:name).new('Ruby')
