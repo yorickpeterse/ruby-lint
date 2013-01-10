02d0b949607fb73cddb3df056b56a1bbe0fb0f48
Yorick Peterse <yorickpeterse@gmail.com>
2013-01-10 10:44:24 +0100

    Performance information for Bacon tests.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- spec/helper.rb
+++ spec/helper.rb
@@ -61,3 +61,22 @@ class Should
     satisfy(desc) { |x| x.__send__(name, *args, &block) }
   end
 end
+
+# Show performance related information after running the tests.
+module Bacon
+  class << self
+    alias old_handle_summary handle_summary
+  end
+
+  def self.handle_summary
+    old_handle_summary
+
+    memory    = `ps -o rss= #{Process.pid}`.strip.to_f
+    memory_mb = (memory / 1024)
+    time_secs = (Time.now - @timer)
+    time_ms   = time_secs * 1000
+
+    puts '%i KB (%.2f MB) used in %.2f miliseconds (%.2f seconds)' \
+      % [memory, memory_mb, time_ms, time_secs]
+  end
+end
