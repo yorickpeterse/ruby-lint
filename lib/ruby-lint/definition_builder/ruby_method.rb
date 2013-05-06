e1910d410c5c3689a87968c619bfe8c7ecc0ba5b
Yorick Peterse <yorickpeterse@gmail.com>
2013-05-06 08:04:47 +0200

    Basic docs for DefinitionBuilder::RubyMethod.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/ruby-lint/definition_builder/ruby_method.rb
+++ lib/ruby-lint/definition_builder/ruby_method.rb
@@ -1,10 +1,20 @@
 module RubyLint
   module DefinitionBuilder
     class RubyMethod < Base
+      ##
+      # Builds the definition for the method definition.
+      #
+      # @return [RubyLint::Definition::RubyMethod]
+      #
       def build
         return new_node([scope])
       end
 
+      ##
+      # Returns the scope to define the method in.
+      #
+      # @return [RubyLint::Definition::RubyObject]
+      #
       def scope
         scope = definitions
 
@@ -19,6 +29,12 @@ module RubyLint
 
       private
 
+      ##
+      # Looks up the receiver used for the method definition.
+      #
+      # @param [RubyLint::AST::Node] node
+      # @return [RubyLint::Definition::RubyObject]
+      #
       def lookup_receiver_definition(node)
         if node.constant?
           return resolve_constant_name(node)
@@ -29,10 +45,17 @@ module RubyLint
         end
       end
 
+      ##
+      # @return [String]
+      #
       def method_name
         return node.children[name_index].to_s
       end
 
+      ##
+      # @param [Array] parents The parent definitions.
+      # @return [RubyLint::Definition::RubyObject]
+      #
       def new_node(parents)
         return Definition::RubyMethod.new(
           :name    => method_name,
@@ -40,10 +63,16 @@ module RubyLint
         )
       end
 
+      ##
+      # @return [TrueClass|FalseClass]
+      #
       def has_receiver?
         return node.type == :defs
       end
 
+      ##
+      # @return [Numeric]
+      #
       def name_index
         return has_receiver? ? 1 : 0
       end
