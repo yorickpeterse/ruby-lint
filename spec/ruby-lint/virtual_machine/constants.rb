7c1efb18f25bb0576efd0c9026cc23b907a516a4
Yorick Peterse <yorickpeterse@gmail.com>
2013-07-16 23:42:05 +0200

    Moved the constant tests in the right block.

--- spec/ruby-lint/virtual_machine/constants.rb
+++ spec/ruby-lint/virtual_machine/constants.rb
@@ -1,29 +1,31 @@
 require File.expand_path('../../../helper', __FILE__)
 
 describe RubyLint::VirtualMachine do
-  # These tests are put in place to ensure that a class/module can refer to
-  # itself without having to look anything up in parent scopes. An example:
-  #
-  #     class Foo
-  #       Foo # => points to the class
-  #     end
-  #
-  should 'add the constant to itself' do
-    code = <<-CODE
-module A
-end
+  describe 'generic constants' do
+    # These tests are put in place to ensure that a class/module can refer to
+    # itself without having to look anything up in parent scopes. An example:
+    #
+    #     class Foo
+    #       Foo # => points to the class
+    #     end
+    #
+    should 'add the constant to itself' do
+      code = <<-CODE
+  module A
+  end
 
-class B
-end
-    CODE
+  class B
+  end
+      CODE
 
-    defs  = build_definitions(code)
-    mod   = defs.lookup(:const, 'A')
-    klass = defs.lookup(:const, 'B')
+      defs  = build_definitions(code)
+      mod   = defs.lookup(:const, 'A')
+      klass = defs.lookup(:const, 'B')
 
-    # `lookup_parent` is set to false to ensure that the code doesn't use
-    # parent scopes to find the constant.
-    mod.lookup(:const, 'A', false).should   == mod
-    klass.lookup(:const, 'B', false).should == klass
+      # `lookup_parent` is set to false to ensure that the code doesn't use
+      # parent scopes to find the constant.
+      mod.lookup(:const, 'A', false).should   == mod
+      klass.lookup(:const, 'B', false).should == klass
+    end
   end
 end
