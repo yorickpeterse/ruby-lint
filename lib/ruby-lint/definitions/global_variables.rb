ab5e9e27055dea72e73f3b237fce4f6ace2ee848
Yorick Peterse <yorickpeterse@gmail.com>
2013-07-17 01:49:00 +0200

    Manually specify the global variables.
    These aren't always the same on the different Ruby implementations. For
    example, Rubinius doesn't by default define global variables such as $1 while
    MRI does.

--- lib/ruby-lint/definitions/global_variables.rb
+++ lib/ruby-lint/definitions/global_variables.rb
@@ -1,3 +1,9 @@
-Kernel.global_variables.each do |name|
+[
+  :$;, :$-F, :$@, :$!, :$SAFE, :$~, :$&, :$`, :$', :$+, :$=, :$KCODE, :$-K,
+  :$,, :$/, :$-0, :$\, :$_, :$stdin, :$stdout, :$stderr, :$>, :$<, :$.,
+  :$FILENAME, :$-i, :$*, :$?, :$$, :$:, :$-I, :$LOAD_PATH, :$",
+  :$LOADED_FEATURES, :$VERBOSE, :$-v, :$-w, :$-W, :$DEBUG, :$-d, :$0,
+  :$PROGRAM_NAME, :$-p, :$-l, :$-a, :$1, :$2, :$3, :$4, :$5, :$6, :$7, :$8, :$9
+].each do |name|
   RubyLint::VirtualMachine.global_scope.define_global_variable(name.to_s)
 end
