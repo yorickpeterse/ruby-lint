5bf090766b2618a13aad8e70b19f5070e4f7f315
Yorick Peterse <yorickpeterse@gmail.com>
2014-01-11 22:41:15 +0100

    Expanded the Rails spec.

--- spec/ruby-lint/virtual_machine/complex/rails_spec.rb
+++ spec/ruby-lint/virtual_machine/complex/rails_spec.rb
@@ -2,16 +2,19 @@ require 'spec_helper'
 
 describe RubyLint::VirtualMachine do
   describe 'rails' do
-    example 'process a basic Rails controller' do
-      code = fixture('complex/rails/basic_controller.rb')
-      defs = build_definitions(code)
+    before :all do
+      code        = fixture('complex/rails/basic_controller.rb')
+      @defs       = build_definitions(code)
+      @controller = @defs.lookup(:const, 'ApplicationController')
+    end
 
-      controller = defs.lookup(:const, 'ApplicationController')
+    example 'inherit the parent controller' do
+      @controller.has_definition?(:instance_method, 'login_required')
+        .should == true
+    end
 
-      controller.is_a?(ruby_object).should == true
-
-      controller.lookup(:instance_method, 'login_required') \
-        .is_a?(ruby_method) \
+    example 'include built-in Rails definitions' do
+      @controller.has_definition?(:method, 'protect_from_forgery')
         .should == true
     end
   end
